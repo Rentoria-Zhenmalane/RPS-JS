6cc5adbb7cc49521037003933134b64216027313
Shane <zhenren1098@gmail.com>
2026-01-21 16:44:23 +0800

    Add autoplay feature to Rock Paper Scissors game
    Introduced an autoplay button and logic in RPS.js to allow the game to play automatically at intervals. Updated RPS.css to style the new autoplay button and its hover/active states. Minor code cleanup and comments added for clarity.

--- RPS.js
+++ RPS.js
@@ -28,6 +28,30 @@ if (!score) {
 
 updateScoreElement(); //Updates score on page load.
 
+let isAutoPlaying = false; //Outside function because the function needs to REMEMBER things between calls.
+let intervalID;
+//These two are called Global Memory, memories that need to be remembered all the time.
+
+function AutoPlaying() { //why not use arrow function here? Beacuse regular function allows hoisting.
+  if (!isAutoPlaying) { //This part only runs because it's true
+    intervalID = setInterval(() => //returns different ID and stored it in variable. Used arror function here
+    {
+      const Move = pickComputerMove();// get random number when called and store in variable called move.
+      playGame(Move); //The result of Move will now pass in playgame function. playGame is now called then generates another random number.  The two randomly generated number will now be compared once the playGame is called.
+    }, 1000);
+
+    isAutoPlaying = true; //This is where it actually turns on
+
+  } else {
+    clearInterval(intervalID);
+    isAutoPlaying = false;
+
+  }
+
+  // If the consition is true then start AutoPlaying else Stop.
+
+}
+
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
@@ -105,19 +129,7 @@ function pickComputerMove() {
   }
   return computerMove;
 
-  //return 'rock'; // returning a value from a function
-  //return; // will return undeifned. However, this will not return anything because we already have a return above. Only the first return will run.
-  //console.log('after');
 
-  // Variables that can be accessed anywhere is called Global Variables
-
-  //Scopes ( {} ) help us avoid naming conflicts.  Whatever variables exist inside the scope only exists inside the scope. -->
-
-
-  //Javascript is case sensitive. Rock is different from rock. Make sure to stay consistent with your naming conventions.
-
-
-  //7:36:33
 }
 
 function handleClick(button, playerMove) {
@@ -131,3 +143,30 @@ function handleClick(button, playerMove) {
 
 
 }
+
+
+
+//return 'rock'; // returning a value from a function
+//return; // will return undeifned. However, this will not return anything because we already have a return above. Only the first return will run.
+//console.log('after');
+
+// Variables that can be accessed anywhere is called Global Variables
+
+//Scopes ( {} ) help us avoid naming conflicts.  Whatever variables exist inside the scope only exists inside the scope. -->
+
+
+//Javascript is case sensitive. Rock is different from rock. Make sure to stay consistent with your naming conventions.
+
+//setInterval starts something, clearInterval stops it, and global state decides which one happens.
+
+/*
+Variables inside a function are erased when the function finishes.
+-So if the value must:
+-persist
+-be remembered
+-be shared between calls
+➡️it CANNOT live inside the function.
+*/
+
+
+//7:36:33
